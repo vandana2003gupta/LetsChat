bf9e86afa0a3e35e2f45b1e7b02c3066349e3ebc
vandana2003gupta <123993348+vandana2003gupta@users.noreply.github.com>
2024-07-29 00:44:53 +0530

    Update Header.jsx

--- client/src/components/chat/menu/Header.jsx
+++ client/src/components/chat/menu/Header.jsx
@@ -1,16 +1,51 @@
 
 import { useContext } from "react";
-import {Box} from "@mui/material";
+import {Box, styled} from "@mui/material";
 import { AccountContext } from "../../context/AccountProvider";
+import {Chat as MessageIcon} from "@mui/icons-material";
+import HeaderMenu from "./HeaderMenu";
+
+const Component = styled(Box)`
+      height: 44px;
+      background-color: #ededed;
+      padding: 8px 16px;
+      display: flex;
+      align-items: center;
+`;
+
+const Wrapper= styled(Box)`
+      margin-left: auto;
+      & > *{
+        margin-left: 2px;
+        padding:8px;
+        color: #000;
+      }
+      & :first-child{
+        font-size: 22px;
+        margin-right: 8px;
+        margin-top: 3px;
+      }
+`;
+
+const Image = styled('img')({
+      height: 42,
+      width: 42,
+      borderRadius : '50%'
+
+})
 
 const Header = () => {
 
    const {account} = useContext(AccountContext);
     return(
         <>
-          <Box>
-              <img src= 'https://static.straitstimes.com.sg/s3fs-public/articles/2020/12/01/af_moneyheist_011220.jpg' alt= "dp"/>
-          </Box>
+          <Component>
+              <Image src= {account.picture} alt= "dp"/>
+              <Wrapper>
+                <MessageIcon/>
+                <HeaderMenu/>
+              </Wrapper>
+          </Component>
         </>
     )
 }
