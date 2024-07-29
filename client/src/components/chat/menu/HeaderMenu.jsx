fd2e060122b6529a023d38fa244ee17ec03e2249
vandana2003gupta <123993348+vandana2003gupta@users.noreply.github.com>
2024-07-29 17:03:54 +0530

    Update HeaderMenu.jsx

--- client/src/components/chat/menu/HeaderMenu.jsx
+++ client/src/components/chat/menu/HeaderMenu.jsx
@@ -1,42 +1,41 @@
+import { useState, useRef } from "react";
+import { MoreVert } from "@mui/icons-material";
+import { Menu, MenuItem, styled } from "@mui/material";
 
-import {MoreVert} from '@mui/icons-material';
-import { useState } from 'react';
-import {Menu, MenuItem} from '@mui/material';
-
-
-
+const Menuoption = styled(Menu)`
+      font-size: 14px;
+      padding: 15px 60px 5px 24px;
+      color: #4a4a4a;
+`
+ 
 const HeaderMenu = () => {
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
 
-    const[open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleClose = () =>{
-        setOpen(false);
-    }
-    const handleClick = () =>{
-        setOpen(true);
-    }
+  const handleClick = () => {
+    setOpen(true);
+  };
 
-
-
-    return (
-      <>
-        <MoreVert onClick = {handleClick} />
-        <Menu
-          anchorEl={open}
-          keepMounted
-          open={open}
-          onClose={handleClose}
-          getContentAnchorE1={null}
-          anchorOrigin={{
-            vertical: 'bottom',
-          }}
-        >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-          <MenuItem onClick={handleClose}>Logout</MenuItem>
-        </Menu>
-      </>
-    );
-}
+  return (
+    <>
+      <MoreVert ref={anchorRef} onClick={handleClick} />
+      <Menu
+        anchorEl={anchorRef.current}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        getContentAnchorEl={null}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </>
+  );
+};
 
 export default HeaderMenu;
